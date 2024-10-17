5eae53c46f4e68a8de5e02af09b6cd745d3c75b1
Allan Reis <allan1985@gmail.com>
2024-10-16 22:09:41 -0300

    Subida Pypi

---
+++ README.md
@@ -0,0 +1,18 @@
+# package_name
+
+Descrição do Sistema Bancário de Allan Reis
+	- 
+	-
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+
+```bash
+pip install sis-bancario-allan
+```
+## Author
+Allan Reis
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="sis-bancario-allan",
+    version="0.0.1",
+    author="Allan_Reis",
+    author_email="my_email",
+    description="Sistema Bancário Allan Reis",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AllanRodrigo/bootcamp-eng-dados-desafio-python/"
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
